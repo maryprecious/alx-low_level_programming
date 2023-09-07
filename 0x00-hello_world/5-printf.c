e567c90db39b521bc63f2d95ab925fb3d68b6f29
maryprecious <maryprecious144@gmail.com>
2023-09-07 13:57:03 -0700

    helloprint

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+
+/**
+ * main " this is the main function
+ * return: 0 when successfuil
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
