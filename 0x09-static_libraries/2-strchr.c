5712b080cb4d3e7a66bed5e1aa35c8f92d235829
maryprecious <maryprecious144@gmail.com>
2023-10-03 07:33:26 -0700

    Create a script called create_static_lib.sh that creates a static library called liball.a from all the .c files that are in the current directory

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,23 +1,19 @@
 #include "main.h"
-#include <stddef.h>
-
 /**
-*_strchr - Returns a pointer to the first occurrence
-*of the character c in the string s, or NULL if the
-*character is not found
-*
-*@s:string targeted
-*@c:character targeted
-*
-*Return: returns pointer to first occcurence of c
-*/
+ * _strchr - the main file
+ * @s: input
+ * @c: input
+ * Return: 0
+ */
 char *_strchr(char *s, char c)
 {
-int i;
-
-for (i = 0; (s[i] != c) && (s[i] != '\0'); i++)
-if (s[i] == c)
-return (s + i);
-else
-return (NULL);
+	while (*s >= '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (0);
 }
