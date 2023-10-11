203488dac841d3c88772abdcc3e679fb45cecfb0
maryprecious <maryprecious144@gmail.com>
2023-10-10 20:24:22 -0700

    a function that creates a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * new_dog - creates new dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: pointer to a new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	unsigned int a, b, c;
+	dog_t *dog;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	for (a = 0; name[a]; a++)
+		;
+	a++;
+	dog->name = malloc(a * sizeof(char));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	for (c = 0; c < a; c++)
+		dog->name[c] = name[c];
+	dog->age = age;
+	for (b = 0; owner[b]; b++)
+		;
+	b++;
+	dog->owner = malloc(b * sizeof(char));
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (c = 0; c < b; c++)
+		dog->owner[c] = owner[c];
+	return (dog);
+}
