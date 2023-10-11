c7c1e9addab6b6f8066d33ddb04f2848250cf6b4
maryprecious <maryprecious144@gmail.com>
2023-10-10 19:30:21 -0700

    Write a program that prints the name of the file it was compiled from, followed by a new line

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print out file name compiled from that is main source code
+ *
+ * Return: int
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
